ff191ae7fdae356dfdaff02b72821b91828b2c48
Jason Etcovitch <jasonetco@github.com>
2018-10-22 20:56:50 -0400

    Move writeGood tests

---
+++ test/lib/providers/write-good.test.js
@@ -0,0 +1,28 @@
+const WriteGood = require('../../../lib/providers/write-good')
+
+describe('WriteGood provider', () => {
+  let provider
+
+  beforeEach(() => {
+    const map = new Map()
+
+    const obj = {
+      'filename.md': '# This is some text',
+      'anotherfile.md': 'this **Has** some Problems. it sure does jason?',
+      'cats.md': 'So the cat was stolen.'
+    }
+
+    for (const key in obj) {
+      map.set(key, obj[key])
+    }
+
+    provider = new WriteGood(map)
+  })
+
+  describe('#buildWriteGoodResults', () => {
+    it('returns the expected result', () => {
+      const actual = provider.buildResults()
+      expect(actual).toMatchSnapshot()
+    })
+  })
+})
