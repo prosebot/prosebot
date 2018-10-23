923f08b6e8b24f6abedaf40b0e1193abd9613019
Jason Etcovitch <jasonetco@github.com>
2018-10-22 21:20:05 -0400

    Build provider

---
+++ test/lib/providers/spellcheck.test.js
@@ -0,0 +1,27 @@
+const SpellCheck = require('../../../lib/providers/spellcheck')
+
+describe('SpellCheck provider', () => {
+  let provider
+
+  beforeEach(() => {
+    const map = new Map()
+
+    const obj = {
+      'filename.md': 'iam not a wurd',
+      'anotherfile.md': 'Tenis is a fun sporq'
+    }
+
+    for (const key in obj) {
+      map.set(key, obj[key])
+    }
+
+    provider = new SpellCheck(map)
+  })
+
+  describe('#buildResults', () => {
+    it('returns the expected result', () => {
+      const actual = provider.buildResults()
+      expect(actual).toMatchSnapshot()
+    })
+  })
+})
